7559906e244b097514eb3cad836ad1b88ac2869b
Riya Devkota <riyadevkota2062@gmail.com>
2026-04-15 17:48:50 +0545

    updated files

--- Intro.py
+++ Intro.py
@@ -1,2 +1,23 @@
 print("Hello world")
 print(5)
+print("Bye bye", 8)
+print(17*5)
+print("""The golden sun begins to setThe golden sun begins to set
+Upon a shore I won't forget
+The waves are soft, the wind is still,
+And peace rests on the distant hill.
+No noisy thought, no rushing street,
+Just steady sand beneath my feet.
+A quiet heart, a gentle breath,
+Life in the moment, conquering death. 
+""")#python does not support multiline so we can use triple """ or\n
+
+#or for multiline comment we can use triple """ or pound too #
+"""
+This is a multi-line comment
+Used for explanation
+Python ignores it if not assigned to a variable"""
+
+print("Hello everyone\nand this viewer is  riya ")
+print("Hey", 4, 5, sep="$" ,end="009\n") # sep connect 3 value with$ and end comes before the sentence is printed before hello world is printed
+print("Hello riya")# without\n tbis line will be printed in same line but with \n this line will be printed in next line
---
+++ variable.py
@@ -0,0 +1,25 @@
+a = 3 + 2j  # complex number having real part and imaginary part
+a2 = complex(2, 3)  # we can declare complex  number in this way to
+b = "Riya"
+c = True
+d = None
+print(a)
+print(b)
+print(c)
+print(d)
+print(a, b, c, d)
+# print(a+b) shows error due to different type is doesnot concatenate
+# python is automatically typed langauge so we dont need to declare using datatype
+print("Type of a is", type(a))
+print("Type of a2 is", type(a2))
+print("Type of b is", type(b))
+print("Type of c is", type(c))
+print("Type of d is", type(d))
+list1 = [[2, 3.2], ["riya,riyan"], [-4, 5]]
+print(list1)
+list = [2, 3]
+print(list)
+tuple = ("Riya, Riyan", (-2, 3), (2.2, 3.3))
+print(tuple)
+dict1 = {"name": "riya", "age": 20, "grade": "A"}
+print(dict1)
