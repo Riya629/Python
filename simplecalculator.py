90fbb51d8a8be35362f0635e6b2c80d226298133
Riya Devkota <riyadevkota2062@gmail.com>
2026-04-16 11:08:50 +0545

    Add simple calculator functionality and additional arithmetic operations

---
+++ simplecalculator.py
@@ -0,0 +1,10 @@
+num1=12
+num2=2
+add=num1+num2
+print("Addition of",num1 ,"and", num2, "is",add)
+sub=num1-num2
+print("subtraction of ",num1 ,"and", num2, "is",sub)
+multi=num1*num2
+print("Multiplication of",num1, "and", num2," is",multi)
+div=num1/num2
+print("Division of",num1, "and", num2,"is",div)
